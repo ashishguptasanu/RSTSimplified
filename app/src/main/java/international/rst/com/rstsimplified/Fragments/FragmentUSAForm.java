6dc6efce022522cf70f158e753d016614330743b
Ashish <ashish.ashish.gupta745@gmail.com>
2017-03-18 14:00:30 +0530

    Applicant File

--- app/src/main/java/international/rst/com/rstsimplified/Fragments/FragmentUSAForm.java
+++ app/src/main/java/international/rst/com/rstsimplified/Fragments/FragmentUSAForm.java
@@ -1,17 +1,68 @@
 package international.rst.com.rstsimplified.Fragments;
-
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-/**
- * Created by Ashish on 18-03-2017.
- */
-
+import international.rst.com.rstsimplified.R;
 public class FragmentUSAForm extends android.support.v4.app.Fragment {
+    String title;
+    View view;
     public static FragmentUSAForm newFormInstance( String title) {
         FragmentUSAForm fragmentUsaForm = new FragmentUSAForm();
         Bundle args = new Bundle();
         args.putString("title", title);
         fragmentUsaForm.setArguments(args);
         return fragmentUsaForm;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        title = getArguments().getString("title");
+        if(title.equalsIgnoreCase("form1")){
+            view = inflater.inflate(R.layout.consult_form,container, false);
+        }
+        else if(title.equalsIgnoreCase("form2")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form3")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form4")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form5")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form6")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form7")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form8")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form9")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form10")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form11")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form12")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form13")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        else if(title.equalsIgnoreCase("form14")){
+            view = inflater.inflate(R.layout.contact_form,container,false);
+        }
+        return view;
     }
 }
