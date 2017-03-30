13b5089aba3c362fd87ccbbacdc9a2c7cb6cece7
Ashish <ashish.ashish.gupta745@gmail.com>
2017-03-30 18:26:30 +0530

    Applicant File

--- app/src/main/java/international/rst/com/rstsimplified/Fragments/FragementUsaVisaType.java
+++ app/src/main/java/international/rst/com/rstsimplified/Fragments/FragementUsaVisaType.java
@@ -35,7 +35,7 @@ public class FragementUsaVisaType extends android.support.v4.app.Fragment{
     View view;
     String title, visaUrl;
     SharedPreferences sharedPreferences;
-    RecyclerView mRecyclerView, mRecyclerView2, mRecyclerView3, mRecyclerView4, mRecyclerView5;
+    public RecyclerView mRecyclerView, mRecyclerView2, mRecyclerView3, mRecyclerView4, mRecyclerView5;
     UsaVisaAdapter1 mAdapter1, mAdapter2, mAdapter3, mAdapter4, mAdapter5;
     RecyclerView.LayoutManager layoutManager1, layoutManager2, layoutManager3, layoutManager4, layoutManager5;
     private List<VisaType_> visaTypes = new ArrayList<>();
@@ -54,67 +54,76 @@ public class FragementUsaVisaType extends android.support.v4.app.Fragment{
         return fragmentUsaVisaType;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         title = getArguments().getString("title");
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
         visaUrl = sharedPreferences.getString("url_usa_visa","");
-        loadVisaType(visaUrl);
+
         System.out.println(visaUrl);
         Bundle bundle  = getArguments();
-        ;
+
         if(title.equalsIgnoreCase("business")){
             view = inflater.inflate(R.layout.fragment_usa_visa1, container, false);
             mRecyclerView = (RecyclerView)view.findViewById(R.id.recycler_usa_visa);
-
-            mAdapter1 = new UsaVisaAdapter1(getContext(), visaTypes1);
-            mRecyclerView.setHasFixedSize(true);
             layoutManager1 = new LinearLayoutManager(getContext());
+            mRecyclerView.setHasFixedSize(true);
             mRecyclerView.setLayoutManager(layoutManager1);
-            mRecyclerView.setAdapter(mAdapter1);
+            loadVisaType1(visaUrl);
+
         }
         else if(title.equalsIgnoreCase("family")){
             view = inflater.inflate(R.layout.fragment_usa_visa2, container, false);
             mRecyclerView2 = (RecyclerView)view.findViewById(R.id.recycler_usa_visa2);
-
-            mAdapter2 = new UsaVisaAdapter1(getContext(), visaTypes2);
-            mRecyclerView2.setHasFixedSize(true);
             layoutManager2 = new LinearLayoutManager(getContext());
+            mRecyclerView2.setHasFixedSize(true);
             mRecyclerView2.setLayoutManager(layoutManager2);
-            mRecyclerView2.setAdapter(mAdapter2);
+            loadVisaType2(visaUrl);
+
+
         }
         else if(title.equalsIgnoreCase("tourist")){
             view = inflater.inflate(R.layout.fragment_usa_visa3, container, false);
             mRecyclerView3 = (RecyclerView)view.findViewById(R.id.recycler_usa_visa3);
-
             mAdapter3 = new UsaVisaAdapter1(getContext(), visaTypes3);
             mRecyclerView3.setHasFixedSize(true);
             layoutManager3 = new LinearLayoutManager(getContext());
             mRecyclerView3.setLayoutManager(layoutManager3);
-            mRecyclerView3.setAdapter(mAdapter3);
+            loadVisaType3(visaUrl);
         }
         else if(title.equalsIgnoreCase("work")){
             view = inflater.inflate(R.layout.fragment_usa_visa4, container, false);
             mRecyclerView4 = (RecyclerView)view.findViewById(R.id.recycler_usa_visa4);
-
             mAdapter4 = new UsaVisaAdapter1(getContext(), visaTypes4);
             mRecyclerView4.setHasFixedSize(true);
             layoutManager4 = new LinearLayoutManager(getContext());
             mRecyclerView4.setLayoutManager(layoutManager4);
-            mRecyclerView4.setAdapter(mAdapter4);
+            loadVisaType4(visaUrl);
         }
-
-
-
-
+        else if(title.equalsIgnoreCase("treaty")){
+            view = inflater.inflate(R.layout.fragment_usa_visa5, container, false);
+            mRecyclerView5 = (RecyclerView)view.findViewById(R.id.recycler_usa_visa5);
+            mAdapter5 = new UsaVisaAdapter1(getContext(), visaTypes5);
+            mRecyclerView5.setHasFixedSize(true);
+            layoutManager5 = new LinearLayoutManager(getContext());
+            mRecyclerView5.setLayoutManager(layoutManager5);
+            loadVisaType5(visaUrl);
+        }
 
         return view;
     }
 
 
 
-    private void loadVisaType(String url) {
+    private void loadVisaType1(String url) {
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
@@ -129,41 +138,217 @@ public class FragementUsaVisaType extends android.support.v4.app.Fragment{
             @Override
             public void onResponse(Call<VisaType> call, Response<VisaType> response) {
                 visaTypes1.clear();
-                visaTypes2.clear();
-                visaTypes3.clear();
-                visaTypes4.clear();
-                visaTypes5.clear();
                 if(response != null){
                     VisaType jsonResponse = response.body();
                     visaTypes = jsonResponse.getVisaType();
                     for(int i=0; i<visaTypes.size();i++){
                         if (visaTypes.get(i).getVisaTypeId() == 1){
                             visaTypes1.add(visaTypes.get(i));
-
-                        }
-                        else if(visaTypes.get(i).getVisaTypeId() == 2){
-                            visaTypes2.add(visaTypes.get(i));
-                        }
-                        else if(visaTypes.get(i).getVisaTypeId() == 3){
-                            visaTypes3.add(visaTypes.get(i));
-                        }
-                        else if(visaTypes.get(i).getVisaTypeId() == 4){
-                            visaTypes4.add(visaTypes.get(i));
-                        }
-                        else if(visaTypes.get(i).getVisaTypeId() == 8){
-                            visaTypes5.add(visaTypes.get(i));
                         }
                     }
-                    /*if(visaTypes5.size() > 0){
+
+
+                    if(visaTypes1 != null){
+                        mAdapter1 = new UsaVisaAdapter1(getContext(), visaTypes1);
                         mRecyclerView.setAdapter(mAdapter1);
-                        mRecyclerView2.setAdapter(mAdapter2);
-                        mRecyclerView3.setAdapter(mAdapter3);
-                        mRecyclerView4.setAdapter(mAdapter4);
+                    }
+                        //mRecyclerView2.setAdapter(mAdapter2);
+                        //mRecyclerView3.setAdapter(mAdapter3);
+                        //mRecyclerView4.setAdapter(mAdapter4);
                         //mRecyclerView5.setAdapter(mAdapter5);
-                    }*/
+
                     //adapter = new VisaTypeAdapter(getApplicationContext(),visaTypes);
                     //recyclerView.setAdapter(adapter);
                 }
+
+                System.out.println(visaTypes.size()+ "  " + visaTypes1.size()+ "  "+ visaTypes2.size()+ "  "+ visaTypes3.size()+ "  "+ visaTypes4.size());
+
+            }
+            @Override
+            public void onFailure(Call<VisaType> call, Throwable t) {
+                Log.v("Error",t.getMessage());
+            }
+        });
+    }
+    private void loadVisaType2(String url) {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.uaevisasonline.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        final VisaResponse request = retrofit.create(VisaResponse.class);
+        Call<VisaType> call = request.getVisaType(url);
+        //
+        call.enqueue(new Callback<VisaType>() {
+            @Override
+            public void onResponse(Call<VisaType> call, Response<VisaType> response) {
+
+                if(response != null){
+                    VisaType jsonResponse = response.body();
+                    visaTypes = jsonResponse.getVisaType();
+                    for(int i=0; i<visaTypes.size();i++){
+                        if (visaTypes.get(i).getVisaTypeId() == 2){
+                            visaTypes2.add(visaTypes.get(i));
+                        }
+                    }
+
+
+                    if(visaTypes2 != null){
+                        mAdapter2 = new UsaVisaAdapter1(getContext(), visaTypes2);
+                        mRecyclerView2.setAdapter(mAdapter2);
+                    }
+                    //mRecyclerView2.setAdapter(mAdapter2);
+                    //mRecyclerView3.setAdapter(mAdapter3);
+                    //mRecyclerView4.setAdapter(mAdapter4);
+                    //mRecyclerView5.setAdapter(mAdapter5);
+
+                    //adapter = new VisaTypeAdapter(getApplicationContext(),visaTypes);
+                    //recyclerView.setAdapter(adapter);
+                }
+
+                System.out.println(visaTypes.size()+ "  " + visaTypes1.size()+ "  "+ visaTypes2.size()+ "  "+ visaTypes3.size()+ "  "+ visaTypes4.size());
+
+            }
+            @Override
+            public void onFailure(Call<VisaType> call, Throwable t) {
+                Log.v("Error",t.getMessage());
+            }
+        });
+    }
+    private void loadVisaType3(String url) {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.uaevisasonline.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        final VisaResponse request = retrofit.create(VisaResponse.class);
+        Call<VisaType> call = request.getVisaType(url);
+        //
+        call.enqueue(new Callback<VisaType>() {
+            @Override
+            public void onResponse(Call<VisaType> call, Response<VisaType> response) {
+                visaTypes3.clear();
+                if(response != null){
+                    VisaType jsonResponse = response.body();
+                    visaTypes = jsonResponse.getVisaType();
+                    for(int i=0; i<visaTypes.size();i++){
+                        if (visaTypes.get(i).getVisaTypeId() == 3){
+                            visaTypes3.add(visaTypes.get(i));
+                        }
+                    }
+
+
+                    if(visaTypes3 != null){
+                        mAdapter3 = new UsaVisaAdapter1(getContext(), visaTypes3);
+                        mRecyclerView3.setAdapter(mAdapter3);
+                    }
+                    //mRecyclerView2.setAdapter(mAdapter2);
+                    //mRecyclerView3.setAdapter(mAdapter3);
+                    //mRecyclerView4.setAdapter(mAdapter4);
+                    //mRecyclerView5.setAdapter(mAdapter5);
+
+                    //adapter = new VisaTypeAdapter(getApplicationContext(),visaTypes);
+                    //recyclerView.setAdapter(adapter);
+                }
+
+                System.out.println(visaTypes.size()+ "  " + visaTypes1.size()+ "  "+ visaTypes2.size()+ "  "+ visaTypes3.size()+ "  "+ visaTypes4.size());
+
+            }
+            @Override
+            public void onFailure(Call<VisaType> call, Throwable t) {
+                Log.v("Error",t.getMessage());
+            }
+        });
+    }
+    private void loadVisaType4(String url) {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.uaevisasonline.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        final VisaResponse request = retrofit.create(VisaResponse.class);
+        Call<VisaType> call = request.getVisaType(url);
+        //
+        call.enqueue(new Callback<VisaType>() {
+            @Override
+            public void onResponse(Call<VisaType> call, Response<VisaType> response) {
+                visaTypes4.clear();
+                if(response != null){
+                    VisaType jsonResponse = response.body();
+                    visaTypes = jsonResponse.getVisaType();
+                    for(int i=0; i<visaTypes.size();i++){
+                        if (visaTypes.get(i).getVisaTypeId() == 4){
+                            visaTypes4.add(visaTypes.get(i));
+                        }
+                    }
+
+
+                    if(visaTypes2 != null){
+                        mAdapter4 = new UsaVisaAdapter1(getContext(), visaTypes4);
+                        mRecyclerView4.setAdapter(mAdapter4);
+                    }
+                    //mRecyclerView2.setAdapter(mAdapter2);
+                    //mRecyclerView3.setAdapter(mAdapter3);
+                    //mRecyclerView4.setAdapter(mAdapter4);
+                    //mRecyclerView5.setAdapter(mAdapter5);
+
+                    //adapter = new VisaTypeAdapter(getApplicationContext(),visaTypes);
+                    //recyclerView.setAdapter(adapter);
+                }
+
+                System.out.println(visaTypes.size()+ "  " + visaTypes1.size()+ "  "+ visaTypes2.size()+ "  "+ visaTypes3.size()+ "  "+ visaTypes4.size());
+
+            }
+            @Override
+            public void onFailure(Call<VisaType> call, Throwable t) {
+                Log.v("Error",t.getMessage());
+            }
+        });
+    }
+    private void loadVisaType5(String url) {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.uaevisasonline.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        final VisaResponse request = retrofit.create(VisaResponse.class);
+        Call<VisaType> call = request.getVisaType(url);
+        //
+        call.enqueue(new Callback<VisaType>() {
+            @Override
+            public void onResponse(Call<VisaType> call, Response<VisaType> response) {
+                visaTypes5.clear();
+                if(response != null){
+                    VisaType jsonResponse = response.body();
+                    visaTypes = jsonResponse.getVisaType();
+                    for(int i=0; i<visaTypes.size();i++){
+                        if (visaTypes.get(i).getVisaTypeId() == 8){
+                            visaTypes5.add(visaTypes.get(i));
+                        }
+                    }
+
+
+                    if(visaTypes5 != null){
+                        mAdapter5 = new UsaVisaAdapter1(getContext(), visaTypes5);
+                        mRecyclerView5.setAdapter(mAdapter5);
+                    }
+                    //mRecyclerView2.setAdapter(mAdapter2);
+                    //mRecyclerView3.setAdapter(mAdapter3);
+                    //mRecyclerView4.setAdapter(mAdapter4);
+                    //mRecyclerView5.setAdapter(mAdapter5);
+
+                    //adapter = new VisaTypeAdapter(getApplicationContext(),visaTypes);
+                    //recyclerView.setAdapter(adapter);
+                }
+
                 System.out.println(visaTypes.size()+ "  " + visaTypes1.size()+ "  "+ visaTypes2.size()+ "  "+ visaTypes3.size()+ "  "+ visaTypes4.size());
 
             }
